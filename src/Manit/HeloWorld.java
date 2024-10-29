e915733b2807fbc3f09fa9b4c8a566b45fb35f66
MANMINHNGUYEN33 <mn7455899@gmail.com>
2024-10-30 00:18:04 +0700

    init project

---
+++ src/Manit/HeloWorld.java
@@ -0,0 +1,11 @@
+package Manit;
+
+public class HeloWorld {
+	public static void main(String[] args) {
+		System.out.println("Heloo Minh man Dep trai");
+		System.out.println("Heloo Minh man Dep trai");
+		System.out.println("Heloo Minh man Dep trai");
+		System.out.println("Heloo Minh man Dep trai");
+	}
+
+}
